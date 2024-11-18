4de7544e885c6bf96132a7d967f34216e8f6e7de
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2024-11-18 15:12:34 -0300

    upgrade
    adicionado novos metodos ao banco

--- database.py
+++ database.py
@@ -28,6 +28,40 @@ class DataBase():
         except AttributeError:
             print("Faça a conexão com o banco")
 
+    # Adicionar alguem ao abnco
+    def insert_user(self, name, user, password, access):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute("""
+                INSERT INTO users(name, user, password, access) VALUES(?,?,?,?)
+            """)(name, user, password, access)
+            self.connection.commit()
+        except AttributeError:
+            print("Faça a conexão ao banco")
+
+    # Metodo para dar check aos usuarios do sistema
+    def check_user(self, user, password):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute("""
+                SELECT * FROM users;
+            """)
+
+            # Metodo para verificar quem fez a mudança 
+            for linha in cursor.fetchall():
+                if linha[2].upper () == user.upper() and linha [3] == password and linha[4] == "Administrador":
+                    return "Administrador"
+                    break
+                elif linha[2].upper () == user.upper() and linha [3] == password and linha[4] == "Usuário":
+                    return "user"
+                    break
+                else:
+                    continue
+            return "sem acesso"
+        except:
+            pass
+
+
 if __name__ == "__main__":
     db = DataBase()
     db.conecta()
